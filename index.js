309f67fa68666aa3f7a772a278e6d08267b49207
anjumhere <codeanjum12@gmail.com>
2026-01-03 16:12:46 +0500

    add files

---
+++ index.js
@@ -0,0 +1,16 @@
+const totalBal = document.getElementById("total-bal");
+const income = document.getElementById("income-balance");
+const expense = document.getElementById("expense-balance");
+const historyList = document.getElementById("history-list");
+const transInput = document.querySelector(".trans-input");
+const transPurpose = document.querySelector(".trans-purpose");
+const addButton = document.querySelector(".add-btn");
+console.log(
+  totalBal,
+  income,
+  expense,
+  historyList,
+  transInput,
+  transPurpose,
+  addButton
+);
